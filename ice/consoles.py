e7ebd0e7137fd9b09d132474f5286401a9782aa6
Michael Ranciglio <rancigliom@gmail.com>
2017-10-09 17:28:04 -0500

    Fix the rest of the higan/icarus bug
    Removed the check for is_file from the consoles page. Why? Well...
    	higan folders are not files, but can be opened!
    	So we treat them as they are a rom!
    Added a check for higan autogen files (files ending on .rom or .ram)
    Works flawlessly with my system currently.
    
    TODO: (maybe) Add a check that only does the higan check if the console
    or emulator is detected as higan or a higan console.

--- ice/consoles.py
+++ ice/consoles.py
@@ -4,6 +4,27 @@ import os
 
 import roms
 
+def path_is_higan_gen(path):
+  """
+  We want to ignore all higan generated files since we can just launch
+  higan roms via the folder (rom_name.sf(c)). These files include:
+    - program.rom
+    - ines.rom
+    - character.rom
+    - cx4.data.rom
+    - save.ram
+  As you can probably tell, there is a common pattern here: they all
+  end with ".rom", so we'll just look for that.
+  NOTE: Except for save.ram, so we'll check for .ram too
+  """
+
+  path_check = path[-4] + path[-3] + path[-2] + path[-1]
+
+  # print path <-- debug info
+  # print path_check
+
+  return (path_check == ".rom" or path_check == ".ram")
+
 def console_roms_directory(configuration, console):
   """
   If the user has specified a custom ROMs directory in consoles.txt then
@@ -22,7 +43,10 @@ def path_is_rom(console, path):
   If a list of extensions is supplied for this console, we check if the path has a valid extension
   If no extensions are defined for this console, we just accept any file
   """
-  if console.extensions == "" and os.path.isfile(path):
+  if path_is_higan_gen(path):
+    return False;
+
+  if console.extensions == "":
     return True
 
   # Normalize the extension based on the things we validly ignore.
